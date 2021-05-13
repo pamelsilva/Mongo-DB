c0d6ef5ce8c882b2e2c4a42c4d0c2f30d6f0441d
pamelsilva <pamelap_silva21@hotmail.com>
2021-05-12 21:19:06 -0300

    adicionando arquivos da aula de Mongo DB

---
+++ models/Carro.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Carro = new mongoose.Schema(
+    {
+    nome: { type: String },
+    marca: { type: mongoose.Types.ObjectId, ref: "Marca"},
+    ano: { type: Number },
+    }
+)
+
+
+module.exports = mongoose.model('Carro', Carro)
---
+++ models/Marca.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const Marca = new mongoose.Schema(
+    {
+        nome: { type: String },
+
+    }, 
+    {
+        collection: "Marca",
+    }
+)
+
+module.exports = mongoose.model('Marca', Marca)
